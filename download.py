ca42ac944582b0c69b217cfb63464a0f22aaa6a2
djt-test <118701314+djt-test@users.noreply.github.com>
2022-12-10 15:37:53 +1000

    Update download.py

--- download.py
+++ download.py
@@ -1,18 +1,13 @@
 import os
 import torch
-from torch import autocast
-from diffusers import DiffusionPipeline, EulerDiscreteScheduler
+from diffusers import StableDiffusionPipeline, EulerDiscreteScheduler
 
 def download_model():
     HF_AUTH_TOKEN = os.getenv("HF_AUTH_TOKEN")
-    repo = 'stabilityai/stable-diffusion-2-1-base'
-    scheduler = EulerDiscreteScheduler.from_pretrained(repo, subfolder="scheduler", prediction_type="v_prediction")
-    model = DiffusionPipeline.from_pretrained(repo, 
-                                              torch_dtype=torch.float16, 
-                                              revision="fp16",
-                                              scheduler=scheduler,
-                                              use_auth_token=HF_AUTH_TOKEN) 
-    
+    model_id = "stabilityai/stable-diffusion-2-1"
+    model = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16, use_auth_token=HF_AUTH_TOKEN)
+    model.scheduler = EulerDiscreteScheduler.from_config(model.scheduler.config)
+
 
 if __name__ == "__main__":
     download_model()
